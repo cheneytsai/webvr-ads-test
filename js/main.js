98c7b1b470f4b3bba4e3f4ff1558955170d1b6da
Cheney Tsai <cheneytsai@cheneytsai-macbookpro2.roam.corp.google.com>
2016-05-10 15:35:34 -0700

    Wire up to Simple URL. Test VRView concept using NYTimes amp page

--- js/main.js
+++ js/main.js
@@ -1,13 +1,70 @@
+
+
 (function() {
     'use strict';
 
 
+    AFRAME.registerComponent('set-box1', {
+      schema: {default:''},
+      init() {
+        const box = document.querySelector('a-box');
+
+        const obj = document.getElementById('obj');
+        this.el.addEventListener('click', () => {
+
+          $.ajax({
+
+            type: 'GET',
+            url: 'https://pubads.g.doubleclick.net/gampad/adx?iu=/35096353/vrtesting&sz=2000x2000&c=12345&t=pos%3Dbottom',
+
+            success: function(data) {
+              var ad_url = $(data).find('.img_ad').attr('src');
+
+              box.setAttribute('src', ad_url);
+              obj.setAttribute('material', 'src: url(' + ad_url +   ')');
+            }
+
+          });    
+        });
+      }
+    });
+
+
+    AFRAME.registerComponent('set-box2', {
+      schema: {default:''},
+      init() {
+        const box = document.querySelector('a-box');
+
+        const obj = document.getElementById('obj');
+        this.el.addEventListener('click', () => {
+
+          $.ajax({
+
+            type: 'GET',
+            url: 'https://pubads.g.doubleclick.net/gampad/adx?iu=/35096353/vrtesting&sz=2000x2000&c=12345&t=pos%3Dtop',
+
+            success: function(data) {
+              var ad_url = $(data).find('.img_ad').attr('src');
+
+              box.setAttribute('src', ad_url);
+              obj.setAttribute('material', 'src: url(' + ad_url +   ')');
+            }
+
+          });    
+        });
+      }
+    });
+
     window.onload = function(){
+      /*
+         const sky = document.querySelector('a-sky');
+         sky.setAttribute('src', 'img/concerthall.jpg');
+*/
 /*
          $.ajax({
 
           type: 'GET',
-          url: 'https://pubads.g.doubleclick.net/gampad/adx?iu=/35096353/amptesting/image/static&sz=300x250&c=12345',
+          url: 'https://pubads.g.doubleclick.net/gampad/adx?iu=/35096353/vrtesting&sz=2000x2000&c=12345',
 
           success: function(data) {
             
@@ -18,7 +75,7 @@
             var index_end = data.substring(index_start).search('"');
 
             var ad_url = data.substring(index_start, index_start+index_end)
-            
+
             $('#ad').attr('src', ad_url);
           }
 
